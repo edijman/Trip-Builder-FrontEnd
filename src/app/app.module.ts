9b2f5631f6854c7e3a9e64546316b1c05aa8824b
Adigun Jide Idris <edijman@gmail.com>
2018-11-07 11:32:28 -0500

    Clean: Initial Angular code.
    Added Trip service component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {TripService} from './services/trip.service'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TripService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/trip.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripService {
+
+  constructor() { }
+
+
+  //get Locations
+  //this will autofill form for select
+
+  //get itinerary: One Way, 
+  // this would collect destination, arrival, trip type:One Way or Round trip, and date leaving, date arriving()
+  //would return itinerary -- yet to be formated
+
+
+  //get intinenary: Round Trip
+  // this would collect destination, arrival, date leaving, 
+  //would return itinerary -- yet to be formated
+}
